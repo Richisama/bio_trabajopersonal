09047f5fd1dba1b73d42fcc8b2a75baee5d966db
Richisama <ricardomn51@gmail.com>
2022-01-21 10:39:58 +0100

    ultimo cambio viernes

--- script_exportar.R
+++ script_exportar.R
@@ -1,3 +1,22 @@
 library(MLDataR)
 tir<-MLDataR::thyroid_disease
 write.csv(tir, file= "dataset_tiroides.csv")
+
+sessionInfo()
+
+# order
+
+head(tir)
+
+order(tir)
+tir2<-tir[order(tir$patient_age),]
+tir_completo<-tir[complete.cases(tir),]
+
+# aggregate, agrupar por grupos y calcular un dato estadístico
+
+aggregate(tir_completo$patient_age,
+        list(tir_completo$ThryroidClass), mean )
+edad_tyr<-aggregate(tir_completo$patient_age,
+          list(tir_completo$ThryroidClass, tir_completo$patient_gender), mean) 
+names(edad_tyr)<-c("Tiroides", "Genero", "Media")
+edad_tyr
